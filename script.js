21b74d1912f2ea21f813dab195c44c874599cf22
martincousseau <55063499+martincousseau@users.noreply.github.com>
2023-01-18 09:21:49 +0100

    site fonctionnel

--- script.js
+++ script.js
@@ -1,17 +1,9 @@
-/*var button = document.getElementById("load-button");
-var imageLoaded = false;
-
-button.addEventListener("click", searchOutput = function () {
-  if (!imageLoaded) {
-    var image = new Image();
-    image.src = "téléchargement.jpg";
-    document.body.appendChild(image);
-    imageLoaded = true;
-  }
-});*/
 
 const loadImageButton = document.querySelector('#load-image-button');
+const predictButton = document.querySelector('#predict-button');
 const image = document.querySelector('#image');
+
+
 
 loadImageButton.addEventListener('click', () => {
     const input = document.createElement('input');
@@ -30,3 +22,36 @@ loadImageButton.addEventListener('click', () => {
         reader.readAsDataURL(file);
     });
 });
+
+predictButton.addEventListener('click', () => {
+  var pred = make_prediction(model,image);
+  pred = pred.dataSync();
+  console.log(pred);
+  var result = 0;
+  for (var i in pred){
+    if(pred[i] == '1'){
+      result = i;
+    }
+     
+  }
+  
+  document.getElementById('display').innerHTML = "Le chiffre est : " + result;
+});
+
+async function loadModel(){
+  model = undefined;
+  model = await tf.loadLayersModel("https://raw.githubusercontent.com/martincousseau/Projet_reco_fonds_marins/main/model.json");
+  console.log("model loaded")
+}
+
+
+loadModel();
+
+
+function make_prediction(model,data) {
+  image_input = tf.browser.fromPixels(data,1);
+  image_input = image_input.expandDims(null);
+  const preds = model.predict(image_input);
+  return preds;
+}
+
